6ca62a853b895b5267c15833bdb057e015b43362
mgluchowski <maciej.gluchowski86@gmail.com>
2026-07-08 11:40:37 -0700

    Add MathJax macro configuration

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,29 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['\\[', '\\]']],
+    processEscapes: true,
+    macros: {
+      R: '\\mathbb{R}',
+      Z: '\\mathbb{Z}',
+      N: '\\mathbb{N}',
+      Q: '\\mathbb{Q}',
+      C: '\\mathbb{C}',
+      pP: '\\mathbb{P}',
+      E: '\\mathbb{E}',
+      Var: '\\operatorname{Var}',
+      Cov: '\\operatorname{Cov}',
+      ind: '\\mathbf{1}',
+      vn: '\\varnothing',
+      eps: '\\varepsilon',
+      cL: '\\mathcal{L}',
+      cD: '\\mathcal{D}',
+      cG: '\\mathcal{G}',
+      cF: '\\mathcal{F}',
+      cP: '\\mathcal{P}',
+      supp: '\\operatorname{supp}',
+      Law: '\\operatorname{Law}',
+      Flip: '\\operatorname{Flip}'
+    }
+  }
+};
