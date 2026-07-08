ccc08708dfce5de5bfcf1b25719864c8bae3b95e
mgluchowski <maciej.gluchowski86@gmail.com>
2026-07-08 14:02:59 -0700

    Restore MathJax macros without delimiter override

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,1 +1,27 @@
-window.MathJax = {};
+window.MathJax = {
+  tex: {
+    processEscapes: true,
+    macros: {
+      R: '\\mathbb{R}',
+      Z: '\\mathbb{Z}',
+      N: '\\mathbb{N}',
+      Q: '\\mathbb{Q}',
+      C: '\\mathbb{C}',
+      pP: '\\mathbb{P}',
+      E: '\\mathbb{E}',
+      Var: '\\operatorname{Var}',
+      Cov: '\\operatorname{Cov}',
+      ind: '\\mathbf{1}',
+      vn: '\\varnothing',
+      eps: '\\varepsilon',
+      cL: '\\mathcal{L}',
+      cD: '\\mathcal{D}',
+      cG: '\\mathcal{G}',
+      cF: '\\mathcal{F}',
+      cP: '\\mathcal{P}',
+      supp: '\\operatorname{supp}',
+      Law: '\\operatorname{Law}',
+      Flip: '\\operatorname{Flip}'
+    }
+  }
+};
